abe9bd40852a2d8e0e40afcdf9cffee2a4263d64
Darya15-4 <yasdarya7@gmail.com>
2025-12-01 01:33:59 +0300

    Yasinskaya, types:TForm, TFormElement, TElementAttrubutes, TElementsAttributes

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,1 +1,22 @@
-export type sum = (a: number, b: number) => number
+
+// тип для самой формы
+export type TForm = HTMLFormElement;
+// тип для элемента формы(только те которые валидируем)
+export type TFormElement =  | HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement;
+
+// тип для массива атрибутов элемента формы
+export type TElementAttributes = {
+        element: TFormElement;
+        name?: string;
+        type?: string;
+        value?: unknown;
+        required?: boolean;
+        min?: number;
+        max?: number;
+        minLength?: number;
+        maxLength?: number;
+}
+
+// тип для массива атрибутов для каждого из элементов формы
+export type TElementsAttributes = TElementAttributes[];
+
